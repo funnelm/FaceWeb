9b3a91f768aea171ed411630dc30d99242a53444
funnelm <markfunnell@Marks-MacBook-Pro.local>
2016-11-07 14:59:54 -0500

    Parse Posts JSON

--- FaceWeb/FeedVC.swift
+++ FaceWeb/FeedVC.swift
@@ -14,7 +14,10 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     
     @IBOutlet weak var tableView: UITableView!
-     @IBOutlet weak var signOutTapped: UIButton!
+    @IBOutlet weak var signOutTapped: UIButton!
+    
+    
+    var posts = [Post]()
     
     
     override func viewDidLoad() {
@@ -27,6 +30,22 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
             
             print(snapshot.value)
             
+            if let snapshot = snapshot.children.allObjects as? [FIRDataSnapshot] {
+                
+                for snap in snapshot {
+                    print("SNAP:\(snap)")
+                    
+                    if let postDict = snap.value as? Dictionary<String,Any> {
+                        let key = snap.key
+                        let post = Post(postKey: key, postData: postDict)
+                        self.posts.append(post)
+                    }
+                    
+                }
+                
+            }
+            self.tableView.reloadData()
+            
         })
         
         // Do any additional setup after loading the view.
@@ -37,11 +56,16 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return posts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let post = posts[indexPath.row]
+        print("MF:\(post.caption)")
+        
         return tableView.dequeueReusableCell(withIdentifier: "UITVC") as! UITVC
+        
     }
     
     
